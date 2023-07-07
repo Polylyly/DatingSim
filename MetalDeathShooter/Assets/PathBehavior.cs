46102d87d9714bb7699d5b18c70d99d84cad8490
lpasquini24 <123428916+lpasquini24@users.noreply.github.com>
2023-07-07 16:53:55 -0400

    Commented my code

--- MetalDeathShooter/Assets/PathBehavior.cs
+++ MetalDeathShooter/Assets/PathBehavior.cs
@@ -5,13 +5,21 @@ using UnityEngine.Tilemaps;
 
 public class PathBehavior : MonoBehaviour
 {
+    // ref to the tilemap
     public Tilemap map;
+    // the tile that will be used for the very end of the path
     public Tile curTile;
+    // the tile used for the trailing parts of the path
     public Tile pathTile;
+    // the tile we replace path tiles with when removing them
     public Tile normalTile;
+
+    //where the path starts
     public Vector3Int startPos;
+    // the boundaries of the path
     public Vector2Int topLeftBound;
     public Vector2Int bottomRightBound;
+    // variable that will store the tile position of the very end of the path
     private Vector3Int curPos;
 
     //the data structure that will store the path on a tile basis
@@ -19,16 +27,17 @@ public class PathBehavior : MonoBehaviour
     //the data structure that will store the path on a world basis
     public List<Vector3> worldPath = new List<Vector3>();
 
-    // Start is called before the first frame update
     void Start()
     {
+        // set things up
         curPos = startPos;
         map.SetTile(startPos, curTile);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        // yes this is a bad way to do this but i dont care that much
         if (Input.GetKeyDown("w"))
         {
             moveCurTile(new Vector3Int(0, 1, 0));
@@ -47,23 +56,41 @@ public class PathBehavior : MonoBehaviour
         }
     }
 
+    // this function moves the very end of the path
+    // amount is a vector3 used to pass a direction to move in
     private void moveCurTile(Vector3Int amount)
     {
+        // checks if we are moving backwards along the path
+        // if so, we can erase the path
         if(tilePath.Count >= 1 && tilePath[tilePath.Count - 1] == curPos + amount)
         {
+            // remove the last item from the list of tile pos
             tilePath.RemoveAt(tilePath.Count - 1);
+            // remove the last item from the list of world pos
             worldPath.RemoveAt(worldPath.Count - 1);
+            // change the tile we used to be at to a regular tile
             map.SetTile(curPos, normalTile);
+            // move the very end of the path
             curPos += amount;
+            // set the new very end of the path to the end of path tile
             map.SetTile(curPos, curTile);
             return;
         }
+        // make sure you can't path over your own path
         if (map.GetTile(curPos + amount).Equals(pathTile)) return;
+        // check that you cant path outside the bounds
         if ((curPos + amount).x < topLeftBound.x || (curPos + amount).x > bottomRightBound.x || (curPos + amount).y > topLeftBound.y || (curPos + amount).y < bottomRightBound.y) return;
-            map.SetTile(curPos, pathTile);
+        
+        //now the code to just regularly move the tile
+
+        //set the current end of the path to a trailing path tile
+        map.SetTile(curPos, pathTile);
+        // add the current end of the path to both lists
         tilePath.Add(curPos);
-        worldPath.Add(map.GetCellCenterWorld(curPos));
+        worldPath.Add(map.GetCellCenterWorld(curPos)); //getcellcenterworld is used to get a world position
+        // move the current position
         curPos += amount;
+        // add in a new tile for the very end of the tile
         map.SetTile(curPos, curTile);
     }
 }
--- MetalDeathShooter/Assets/PathFollow.cs
+++ MetalDeathShooter/Assets/PathFollow.cs
@@ -2,25 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+// almost all of this code is ripped from an old pathfinding script i have
+// it follows a path defined by a list of vector3s
+/// </summary>
 public class PathFollow : MonoBehaviour
 {
+    // this is a reference to the pathBehavior script that we will be getting path data from
     public PathBehavior path;
+    // dumb little boolean flag so i can start the agent by pressing space
     bool started = false;
+    // boolean flag so we stop pathfinding at the end of the path
     bool pathCompleted = false;
     public Rigidbody2D rb;
+    // speed defines how fast the agent follows the path
     public float speed;
+    // when the agent is within this distance of the next point on the path, it stops pathfinding to that point and starts going to the next one
     public float nextWaypointDistance;
+    // used to track which position in the list we are pathfinding to
     private int currentWaypoint;
+    // will store path data
     List<Vector3> p;
-    // Start is called before the first frame update
+
     void Start()
     {
+        // load path data from the pathmanager script
          p = path.worldPath;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // dumb little boolean flag so i can start the agent with space
         if (Input.GetKeyDown(KeyCode.Space))
         {
             started = true;

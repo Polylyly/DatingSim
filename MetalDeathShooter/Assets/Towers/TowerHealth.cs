865aac15fb3752150cbd96964412761c8e5c63d9
Polylyly <polybius404@gmail.com>
2023-07-08 16:22:05 +0200

    TowerHealth

---
+++ MetalDeathShooter/Assets/Towers/TowerHealth.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerHealth : MonoBehaviour
+{
+    public float maxHealth, currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHealth >= 0)
+        {
+            //Death
+        }
+    }
+
+    public void DamageTower(float damage)
+    {
+        currentHealth -= damage;
+    }
+}

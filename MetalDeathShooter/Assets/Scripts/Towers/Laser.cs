5961181a8607494396dd8a6a695f061fbce2520c
Polylyly <polybius404@gmail.com>
2023-07-08 20:45:01 +0200

    KILL THE TOWERS

--- MetalDeathShooter/Assets/Scripts/Towers/Laser.cs
+++ MetalDeathShooter/Assets/Scripts/Towers/Laser.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     public int dps;
+    bool Colliding;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        //collision.gameObject.GetComponent<EnemyHealth>().DamageEnemy(dps * Time.deltaTime);
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            StartCoroutine(Damage(collision.gameObject));
+            Colliding = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            StopCoroutine(Damage(collision.gameObject));
+            Colliding = false;
+        }
+    }
+
+    IEnumerator Damage(GameObject Enemy)
+    {
+        yield return new WaitForSeconds(0.5f);
+        Enemy.GetComponent<HealthManager>().DealDamage(dps);
+        if (Colliding) StartCoroutine(Damage(Enemy));
     }
 }
